2c39411e6b14afcf36de992fc7a7e369e588fca0
fay-ff <faylmffei@gmail.com>
2024-10-22 23:22:46 +0800

    Add files via upload

--- server/data/restaurants.js
+++ server/data/restaurants.js
@@ -1,30 +1,46 @@
-// Fill this in
-let restaurantData = [
-
+// Restaurant data array
+export let restaurantData = [
+  {
+    id: 0,
+    name: "My Restaurant",
+    phone: "(415) 123222",
+    address: "63th, changan street",
+    photo: "https://picsum.photos/150/150"
+  },
+  {
+    id: 1,
+    name: "Another Restaurant",
+    phone: "(415) 123-4567",
+    address: "123 Main St, San Francisco, CA 94132",
+    photo: "https://picsum.photos/150/150"
+  }
 ];
 
-let lastId = restaurantData.length;
 
-const getNextId = () => {
-    lastId += 1;
-    return lastId;
+function getNextId() {
+  return restaurantData.length ? restaurantData[restaurantData.length - 1].id + 1 : 0;
 }
 
-// Get a list of restaurants
-const getRestaurants = () => {
-};
+// Get all restaurants
+export function getRestaurants() {
+  return restaurantData;
+}
 
+// Get a restaurant by ID
+export function getRestaurant(id) {
+  return restaurantData.find(restaurant => restaurant.id === id);
+}
 
-// Get a restaurant by id
-const getRestaurant = (id) => {
-};
+// Create a new restaurant
+export function createRestaurant(newRestaurant) {
+  const id = getNextId(); 
+  const restaurant = { id, ...newRestaurant };
+  restaurantData.push(restaurant);  
+  return restaurant;
+}
 
-// Create a new restaurant entry
-const createRestaurant = (newRestaurant) => {
-};
-
-// Delete a restaurant by id
-const deleteRestaurant = (id) => {
-};
-
-export { getRestaurants, getRestaurant, createRestaurant, deleteRestaurant };
+// Delete a restaurant by ID
+export function deleteRestaurant(id) {
+  restaurantData = restaurantData.filter(restaurant => restaurant.id !== id);
+  return restaurantData;
+}
